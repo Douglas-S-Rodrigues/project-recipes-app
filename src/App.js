aaec697bcd66269dd923117e218fcea99badd14c
Janiele Vilar <vilarjaniele@gmail.com>
2022-04-26 18:10:05 -0300

    Iniciando 2,3,4 e 5

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import RecipesProvider from './context/RecipesProvider';
+import Login from './Components/Login';
 
 function App() {
   return (
     <RecipesProvider>
       <h1>Receitas</h1>
+      <Login />
     </RecipesProvider>
 
   );
--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,5 +1,47 @@
-function Login() {
+import React, { useContext, useState } from 'react';
+import RecipesContext from '../context/RecipesContext';
 
+function Login() {
+  const {user, setUser} = useContext(RecipesContext);
+  const [disable, setDisable] = useState(true);
+
+  const validUser = () => {
+    const userEmail = user.email;
+    const userPassword = user.password;
+    const MAX_LENGTH_PASSWORD = 6;
+    const isValidPassword = userPassword >= MAX_LENGTH_PASSWORD;
+    const isValidEmail = userEmail.match(/[\w.!#$%&'*+=?^_`{|}~-]+@[\w.-]+\.[A-Z]{2,}/gmi);
+    setDisable(!(isValidPassword && isValidEmail));
+  }
+
+  return (
+    <section>
+      <input
+        type="email"
+        placeholder="Email"
+        name="email"
+        data-testid="email-input"
+        value={ user.email }
+        onChange={ ({ target }) => setUser({ email: target.value }) }
+      />
+      <input
+        type="password"
+        placeholder="Senha"
+        name="password"
+        data-testid="password-input"
+        value={ user.password }
+        onChange={ ({ target }) =>  setUser({ password: target.value })}
+      />
+      <button
+        type="button"
+        data-testid="login-submit-btn"
+        disabled={ disable }
+        onClick={  }
+      >
+        Enter
+      </button>
+    </section>
+  );
 }
 
 export default Login;
