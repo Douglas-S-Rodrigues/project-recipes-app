f4d5ac8cb271f8027cac1561cb896c7bc50ed3d8
Douglas Rodrigues <douglas.rodrigues.fw@gmail.com>
2022-04-26 16:03:48 -0300

    Adiciona api, context e provider

---
+++ src/context/RecipesProvider.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import RecipesContext from './RecipesContext';
+
+function RecipesProvider({ children }) {
+  const contextValue = {
+
+  };
+
+  return (
+    <RecipesContext.Provider value={ contextValue }>
+      { children }
+    </RecipesContext.Provider>
+  );
+}
+
+RecipesProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default RecipesProvider;
