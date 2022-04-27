25814a5b23f79beb2242954e1160cfe994ee9cba
RegislaineRegis <nanafidelis@gmail.com>
2022-04-26 21:00:56 -0300

    realizaçao do requisito 8

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
+import { Switch, Route } from 'react-router-dom';
 import RecipesProvider from './context/RecipesProvider';
 import Login from './Components/Login';
 import Recipes from './Components/Recipes';
 
 function App() {
   return (
-    <RecipesProvider>
-      <Login />
-      <Recipes />
-    </RecipesProvider>
+    <BrowserRouter>
+      <Switch>
+        <RecipesProvider>
+          <Route path="Recipes" component={ Recipes } />
+          <Route exact path="/" component={ Login } />
+          <Recipes />
+        </RecipesProvider>
+      </Switch>
+    </BrowserRouter>
 
   );
 }
