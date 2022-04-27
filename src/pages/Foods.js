36a851aa1af80784e25c310d4719a7b11e9668e8
Charles Xavier Saldanha <94417307+Charles-XavierS@users.noreply.github.com>
2022-04-27 19:18:11 -0300

    Update Foods.js

--- src/pages/Foods.js
+++ src/pages/Foods.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import Header from '../Components/Header';
+import Footer from '../Components/Footer';
 
 function Foods() {
   return (
-    <h1>Foods</h1>
+    <>
+      <Header />
+      <h1>Foods</h1>
+      <Footer />
+    </>
   );
 }
 
