67731ac6ba7af6cdc2be4b0c253461015fd02646
Charles Xavier Saldanha <94417307+Charles-XavierS@users.noreply.github.com>
2022-04-28 02:35:47 -0300

    Update Profile.js

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import Footer from '../components/Footer';
-import Header from '../components/Header';
+import Footer from '../Components/Footer';
+import Header from '../Components/Header';
 
 function Profile() {
   return (
