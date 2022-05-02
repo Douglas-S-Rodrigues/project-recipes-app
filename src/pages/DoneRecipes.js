50a8f82191bb0c72a8d0d2c08e7398f92400fc8a
Charles-XavierS <charlesxsaldanha@hotmail.com>
2022-05-02 20:49:27 -0300

    Tela de profile

---
+++ src/pages/DoneRecipes.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Header from '../Components/Header';
+
+function DoneRecipes() {
+  return (
+
+    <Header title="Done Recipes" />
+
+  );
+}
+
+export default DoneRecipes;
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Footer from '../Components/Footer';
 import Header from '../Components/Header';
 
@@ -6,6 +7,39 @@ function Profile() {
   return (
     <>
       <Header title="Profile" />
+
+      <h2 data-testid="profile-email">
+        { JSON.parse(window.localStorage.getItem('user')).email }
+      </h2>
+
+      <Link to="/done-recipes">
+        <button
+          type="button"
+          data-testid="profile-done-btn"
+        >
+          Done Recipes
+        </button>
+      </Link>
+
+      <Link to="/favorite-recipes">
+        <button
+          type="button"
+          data-testid="profile-favorite-btn"
+        >
+          Favorite Recipes
+        </button>
+      </Link>
+
+      <Link to="/">
+        <button
+          type="button"
+          onClick={ () => localStorage.clear() }
+          data-testid="profile-logout-btn"
+        >
+          Logout
+        </button>
+      </Link>
+
       <Footer />
     </>
   );
